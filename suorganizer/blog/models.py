9be0d43cbb53a50fa8526a040484eed66f444280
Ryan Boehm <ryan.w.boehm@gmail.com>
2019-06-15 22:20:30 -0400

    set up post model in blog app

--- suorganizer/blog/models.py
+++ suorganizer/blog/models.py
@@ -4,3 +4,8 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=63)
+    slug = models.SlugField()
+    text = models.TextField()
+    pub_data = models.DateField()
