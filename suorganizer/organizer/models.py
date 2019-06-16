1b46f453b25e7f9ef94c8cb4dd2a5e401a8afbb6
Ryan Boehm <ryan.w.boehm@gmail.com>
2019-06-15 22:32:20 -0400

    set up tag model

--- suorganizer/organizer/models.py
+++ suorganizer/organizer/models.py
@@ -4,3 +4,13 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=31)
+    slug = models.SlugField()
+
+class Startup(models.Model):
+    pass
+
+class NewsLink(models.Model):
+    pass
