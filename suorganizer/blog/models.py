1cfc63d170b588e47103439aa84d4ce941a86466
Ryan Boehm <ryan.w.boehm@gmail.com>
2019-06-17 23:31:34 -0400

    state our interests, work to imrpove situation to meet goals

--- suorganizer/blog/models.py
+++ suorganizer/blog/models.py
@@ -7,8 +7,16 @@ from organizer.models import Startup, Tag
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=63)
-    slug = models.SlugField()
+    slug = models.SlugField(
+        max_length=63,
+        help_text='A label for URL config',
+        unique_for_month='pub_date')
     text = models.TextField()
-    pub_data = models.DateField()
-    tags = models.ManyToMany(Tag)
-    startups = models.ManyToMany(Startup)
+    pub_date = models.DateField(
+        'date published',
+        auto_now_add=True)
+    tags = models.ManyToMany(
+        Tag,
+        related_name='blog_posts')
+    startups = models.ManyToMany(
+        Startup, related_name='blog_posts')
