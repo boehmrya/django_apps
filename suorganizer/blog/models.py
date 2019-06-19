55ee789b664428791e3553e7617e17824b1b537d
Ryan Boehm <ryan.w.boehm@gmail.com>
2019-06-18 21:23:23 -0400

    added string and meta info to models

--- suorganizer/blog/models.py
+++ suorganizer/blog/models.py
@@ -15,8 +15,18 @@ class Post(models.Model):
     pub_date = models.DateField(
         'date published',
         auto_now_add=True)
-    tags = models.ManyToMany(
+    tags = models.ManyToManyField(
         Tag,
         related_name='blog_posts')
-    startups = models.ManyToMany(
+    startups = models.ManyToManyField(
         Startup, related_name='blog_posts')
+
+    def __str__(self):
+        return "{} on {}".format(
+            self.title,
+            self.pub_date.strftime('%Y-%m-%d'))
+
+    class Meta:
+        verbose_name = 'blog post'
+        ordering = ['-pub_date', 'title']
+        get_latest_by = 'pub_date'
--- suorganizer/suorganizer/urls.py
+++ suorganizer/suorganizer/urls.py
@@ -16,8 +16,6 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
-from helloworld.views import greeting
-
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 ]
