dcfcef674cb38a79fc4dd01efc2065b64b3b7c3c
Ryan Boehm <ryan.w.boehm@gmail.com>
2019-06-28 00:03:35 -0400

    added 404 exception for non existant tags

--- suorganizer/suorganizer/urls.py
+++ suorganizer/suorganizer/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from organizer.views import homepage
+from organizer.views import homepage, tag_detail
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
