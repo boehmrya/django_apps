ba32b179ee9d5d08afba3e421accbadbd17c003d
Ryan Boehm <ryan.w.boehm@gmail.com>
2019-06-28 01:38:50 -0400

    converted views to use render function

--- suorganizer/organizer/views.py
+++ suorganizer/organizer/views.py
@@ -1,23 +1,17 @@
 
-from django.http.response import (Http404, HttpResponse)
-
+from django.shortcuts import (get_object_or_404, render)
 from .models import Tag
 
-from django.template import Context, loader
-
 def homepage(request):
-    tag_list = Tag.objects.all()
-    template = loader.get_template('organizer/tag_list.html')
-    context = Context({'tag_list': tag_list})
-    output = template.render(context)
-    return HttpResponse(output)
+    return render(
+        request,
+        'organizer/tag_list.html',
+        {'tag_list': Tag.objects.all()})
 
 
 def tag_detail(request, slug):
-    try:
-        tag = Tag.objects.get(slug__iexact=slug)
-    except Tag.DoesNotExist:
-        raise Http404
-    template = loader.get_template('organizer/tag_detail.html')
-    context = Context({'tag': tag})
-    return HttpResponse(template.render(context))
+    tag = get_object_or_404(Tag, slug__iexact=slug)
+    return render(
+        request,
+        'organizer/tag_detail.html',
+        {'tag': tag})
