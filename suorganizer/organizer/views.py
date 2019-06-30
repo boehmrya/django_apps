7bacfbaae364edb1c1d15464d452ffbb04a149e5
Ryan Boehm <ryan.w.boehm@gmail.com>
2019-06-29 23:21:27 -0400

    added view for startup list and detail

--- suorganizer/organizer/views.py
+++ suorganizer/organizer/views.py
@@ -1,8 +1,21 @@
 
 from django.shortcuts import (get_object_or_404, render)
-from .models import Tag
+from .models import Startup, Tag
 
-def homepage(request):
+def startup_list(request):
+    return render(
+        request,
+        'organizer/startup_list.html',
+        {'startup_list': Startup.objects.all()})
+
+def startup_detail(request, slug):
+    startup = get_object_or_404(Startup, slug__iexact=slug)
+    return render(
+        request,
+        'organizer/startup_detail.html',
+        {'startup': startup})
+
+def tag_list(request):
     return render(
         request,
         'organizer/tag_list.html',
