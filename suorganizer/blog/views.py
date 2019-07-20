1f359c7c0ac3d7c332e45a9672d7d367d4160df0
Ryan Boehm <ryan.w.boehm@gmail.com>
2019-07-20 00:12:14 -0400

    added get_object method to PostUpdate class

--- suorganizer/blog/views.py
+++ suorganizer/blog/views.py
@@ -50,12 +50,15 @@ class PostUpdate(View):
     model = Post
     template_name = 'blog/post_form_update.html'
 
-    def get(self, request, year, month, slug):
-        post = get_object_or_404(
+    def get_object(self, year, month, slug):
+        return get_object_or_404(
                 self.model,
                 pub_date__year = year,
                 pub_date__month = month,
                 slug = slug)
+
+    def get(self, request, year, month, slug):
+        post = self.get_object(year, month, slug)
         context = {
             'form': self.form_class(instance = post),
             'post': post,
@@ -63,12 +66,14 @@ class PostUpdate(View):
         return render(request, self.template_name, context)
 
     def post(self, request, year, month, slug):
-        post = get_object_or_404(
-                self.model,
-                pub_date__year = year,
-                pub_date__month = month,
-                slug = slug)
+        post = self.get_object(year, month, slug)
         bound_form = self.form_class(request.POST, instance = post)
         if bound_form.is_valid():
             new_post = bound_form.save()
             return redirect(new_post)
+        else:
+            context = {
+                'form': bound_form,
+                'post': post,
+            }
+            return render(request, self.template_name, context)
