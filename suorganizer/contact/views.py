1a1d41e8d73c24f527b9a9acaa84aaace78e3921
Ryan Boehm <ryan.w.boehm@gmail.com>
2019-07-30 22:47:32 -0400

    added post method to contact form view

--- suorganizer/contact/views.py
+++ suorganizer/contact/views.py
@@ -14,3 +14,15 @@ class ContactView(View):
         return render(request,
                     self.template_name,
                     {'form': self.form_class()})
+
+    def post(self, request):
+        bound_form = self.form_class(request.POST)
+        if bound_form.is_valid():
+            mail_sent = bound_form.send_mail()
+            if mail_sent:
+                # shortcut for add_message
+                success(request, 'Email successfully sent.')
+                return redirect('blog_post_list')
+        return render(request,
+                    self.template_name,
+                    {'form': bound_form})
